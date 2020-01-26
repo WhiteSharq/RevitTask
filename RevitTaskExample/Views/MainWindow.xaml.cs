9d1f637e9d82b891659c5d95b699bd437414c07c
Igor Serdyukov <sis000@mail.ru>
2020-01-26 11:26:25 +0300

    [add] initial

---
+++ RevitTaskExample/Views/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Windows;
+
+namespace RevitTaskExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            InitializeComponent();
+        }
+
+        public MainWindow(MainViewModel viewModel) : this()
+        {
+            var id = Thread.CurrentThread.ManagedThreadId;
+            DataContext = viewModel;
+        }
+    }
+}
